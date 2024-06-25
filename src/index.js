08511210271e3ec5cc9428d4002494c331e36c2d
muhammadisa226 <muhammadisa226@gmail.com>
2024-06-26 01:35:41 +0700

    remove swagger

--- src/index.js
+++ src/index.js
@@ -5,25 +5,18 @@ import morgan from 'morgan'
 import 'dotenv/config'
 import router from './routes/index.js'
 import cookieParser from 'cookie-parser'
-import swaggerUi from 'swagger-ui-express'
-import swaggerDoc from './utils/api-docs.js'
 import { connectDatabase } from './utils/prisma.js'
 
 const port = process.env.PORT_APP || 5000
 const app = express()
-const options = {
-    customCss: '.swagger-ui .topbar { display: none }',
-    customSiteTitle: 'Refood Restfull API Documentation',
-}
 const clientUrl = process.env.CLIENT_URL
 app.use(
     cors({
         credentials: true,
-        origin: [clientUrl, 'http://localhost:4173'],
+        origin: [clientUrl],
     })
 )
 connectDatabase()
-app.use('/docs-api', swaggerUi.serve, swaggerUi.setup(swaggerDoc, options))
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(express.json())
