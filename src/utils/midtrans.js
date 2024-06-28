c60eea2e75a896580d2cbe45b9582ffab21e8240
muhammadisa226 <muhammadisa226@gmail.com>
2024-06-28 09:45:55 +0700

    remove midtrans client prod

--- src/utils/midtrans.js
+++ src/utils/midtrans.js
@@ -1,18 +1,11 @@
 require('dotenv/config');
 const midtransClient = require('midtrans-client');
 
-const isProduction = process.env.NODE_ENV === 'production';
-let clientKey, serverKey;
-if (isProduction) {
-    clientKey = process.env.PROD_CLIENT_KEY;
-    serverKey = process.env.PROD_SERVER_KEY;
-} else {
-    clientKey = process.env.DEV_CLIENT_KEY;
-    serverKey = process.env.DEV_SERVER_KEY;
-}
+clientKey = process.env.MIDTRANS_CLIENT_KEY;
+serverKey = process.env.MIDTRANS__SERVER_KEY;
 
 const snapMidtrans = new midtransClient.Snap({
-    isProduction: isProduction,
+    isProduction: false,
     serverKey: serverKey,
     clientKey: clientKey,
 });
